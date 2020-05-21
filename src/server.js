e3ab7613857d8848b086548f2eb2b4baab798e5a
刘文壮 <liuwenzhuang@corp.netease.com>
2020-05-21 22:04:42 +0800

    chore(workspace): workspace backbone

---
+++ src/server.js
@@ -0,0 +1,28 @@
+import express from 'express'
+import { json, urlencoded } from 'body-parser'
+import cors from 'cors'
+import morgan from 'morgan'
+
+export const app = express()
+
+app.use(cors())
+app.use(json())
+app.use(urlencoded({ extended: true }))
+app.use(morgan('dev'))
+
+app.get('/', (req, res) => {
+  res.send({
+    message: 'hello',
+  })
+})
+
+app.post('/', (req, res) => {
+  console.log('receive req body:', req.body)
+  res.send(200)
+})
+
+export const start = () => {
+  app.listen(1234, 'localhost', () => {
+    console.log('server is on http://localhost:1234')
+  })
+}
